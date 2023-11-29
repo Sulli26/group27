1b7fc2f290969000064ac99b70ae4991e2be03b6
Roshan Patel <149646948+RoshnP2@users.noreply.github.com>
2023-11-29 11:54:23 -0600

    Add files via upload

---
+++ InventoryClass.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+class Inventory:
+    def __init__(self, databaseName, tableName):
+        self.databaseName = databaseName
+        self.tableName = tableName
+
+    def Inventory(self):
+
+        conn = sqlite3.connect(self.databaseName)
+        cursor = conn.cursor()
+
+        cursor.execute(f"""CREATE TABLE IF NOT EXISTS{self.tableName}(
+                       "Title" TEXT NOT NULL,
+                       "ISBN" INTEGER NOT NULL UNIQUE,
+                       "Stock" INTEGER NOT NULL,
+                       "Pages" INTEGER NOT NULL,
+                       "Genre" TEXT NOT NULL,
+                       "ReleaseDate" TEXT NOT NULL,
+                       PRIMARY KEY("ISBN")
+        )""")
+        conn.commit()
+        conn.close()
+
+    def view_Inventory(self):
+        
+        conn = sqlite3.connect(self.databaseName)
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM {self.table_name}")
+        rows = cursor.fetchall()
+        conn.close()
+
+        for row in rows:
+            print(row)
+
+    def search_inventory(self, title):
+        conn = sqlite3.connect(self.database_name)
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM {self.table_name} WHERE Title LIKE ?", ('%' + title + '%',))
+        rows = cursor.fetchall()
+        conn.close()
+
+        if rows:
+            for row in rows:
+                print(row)
+        else:
+            print(f"No results found for title: {title}")
+
+    def decrease_stock(self, ISBN):
+        conn = sqlite3.connect(self.database_name)
+        cursor = conn.cursor()
+        cursor.execute(f"UPDATE {self.table_name} SET Stock = Stock - 1 WHERE ISBN = ?", (ISBN,))
+        conn.commit()
+        conn.close()
