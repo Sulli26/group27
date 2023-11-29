016f1032f4f21d05a5c40229c12be0879a4463a8
seeisalty <141163010+seeisalty@users.noreply.github.com>
2023-11-29 17:09:06 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,163 +1,114 @@
-# main.py
-
-import sqlite3
-import os
+from inventory import Inventory
 from user import User
 from cart import Cart
-from inventory import Inventory
 
-class Menu:
-    def __init__(self):
-        self.conn = sqlite3.connect('group27_Database.db')
-        self.c = self.conn.cursor()
-        self.user_instance = User('group27_Database.db', 'Users')
-        self.cart_instance = Cart('group27_Database.db', 'Cart')
-        self.inventory_instance = Inventory('group27_Database.db', 'Inventory')
+def before_login_menu():
+    while True:
+        print("\nBefore Login Menu:")
+        print("1. Login")
+        print("2. Create Account")
+        print("3. Quit")
 
-    def run(self):
-        while True:
-            print("\n\tPlease choose an option:")
-            print("1. Log In")
-            print("2. Create account")
-            print("3. Quit")
-            choice = input("Enter your choice (1-3): ")
+        choice = input("Enter your choice (1-3): ")
 
-            if choice.isdigit():
-                choice = int(choice)
-                if 1 <= choice <= 3:
-                    pass
-                else:
-                    print("Invalid choice. Please enter a number between 1 and 3.")
+        if choice == '1':
+            login()
+        elif choice == '2':
+            create_account()
+        elif choice == '3':
+            quit_program()
+        else:
+            print("Invalid choice. Please enter a number between 1 and 3.")
 
-            if choice == 1:
-                os.system('cls')
-                print("\n\tLog In selected.")
-                usern = input("Username: ")
-                passw = input("Password: ")
-                loggedIn = self.user_instance.login(usern, passw)
-                while loggedIn:
-                    uID = self.c.execute("SELECT UserID FROM Users WHERE Username = ?", (usern,))
-                    uID = self.c.fetchone()
+def after_login_menu(user, inventory, cart):
+    while True:
+        print("\nAfter Login Menu:")
+        print("1. Logout")
+        print("2. View Account Information")
+        print("3. Inventory Information")
+        print("4. Cart Information")
 
-                    while True:
-                        print("\n\tPlease choose an option")
-                        print("1. Edit Account Information")
-                        print("2. Shop")
-                        print("3. View Cart Information")
-                        print("4. View Order History")
-                        print("5. View profile")
-                        print("6. Delete Profile")
-                        print("7. Log Out")
-                        logInChoice = input("Enter your choice (1-7): ")
+        choice = input("Enter your choice (1-4): ")
 
-                        if logInChoice.isdigit():
-                            logInChoice = int(logInChoice)
-                            if 1 <= logInChoice <= 6:
-                                pass
-                            else:
-                                print("Invalid choice. Please enter a number between 1 and 6.")
+        if choice == '1':
+            user.logout()
+            print("Logged out successfully.")
+            before_login_menu()
+        elif choice == '2':
+            user.viewAccountInformation()
+        elif choice == '3':
+            inventory_menu(inventory, cart)
+        elif choice == '4':
+            cart_menu(cart, inventory)
+        else:
+            print("Invalid choice. Please enter a number between 1 and 4.")
 
-                        if logInChoice == 1:
-                            os.system('cls')
-                            print("\n\tEdit Account Information selected")
-                            print("\n\tPlease choose an option")
+def inventory_menu(inventory, cart):
+    while True:
+        print("\nInventory Information Menu:")
+        print("1. View Inventory")
+        print("2. Search Inventory")
+        print("3. Go Back")
 
-                            while True:
-                                print("\n1. Edit first name")
-                                print("2. Edit last name")
-                                print("3. Edit phone number")
-                                print("4. Edit card information")
-                                print("5. Edit address information")
-                                print("6. Go Back")
-                                editChoice = input("Enter your choice (1-6): ")
-                                if editChoice.isdigit():
-                                    editChoice = int(editChoice)
-                                    if 1 <= editChoice <= 6:
-                                        pass
-                                    else:
-                                        print("Invalid choice. Please enter a number between 1 and 6.")
-                                        continue
+        choice = input("Enter your choice (1-3): ")
 
-                                if editChoice == 1:
-                                    new_name = input("\nEnter your updated first name: ")
-                                    self.c.execute('''UPDATE Users SET First_Name = ? WHERE Username = ?''', (new_name, usern))
-                                    self.conn.commit()
-                                    print("First name has been updated.")
-                                    break
+        if choice == '1':
+            inventory.viewInventory()
+        elif choice == '2':
+            title = input("Enter the title to search: ")
+            inventory.searchInventory(title)
+        elif choice == '3':
+            return
+        else:
+            print("Invalid choice. Please enter a number between 1 and 3.")
 
-                                elif editChoice == 2:
-                                    new_name = input("\nEnter your updated last name: ")
-                                    self.c.execute('''UPDATE Users SET Last_Name = ? WHERE Username = ?''', (new_name, usern))
-                                    self.conn.commit()
-                                    print("Last name has been updated.")
-                                    break
-                                    
-                                # Add similar logic for other edit options
+def cart_menu(cart, inventory):
+    while True:
+        print("\nCart Information Menu:")
+        print("1. View Cart")
+        print("2. Add Items to Cart")
+        print("3. Remove an Item from Cart")
+        print("4. Check Out")
+        print("5. Go Back")
 
-                        elif logInChoice == 2:
-                            os.system('cls')
-                            print("\n\tShop selected\n")
+        choice = input("Enter your choice (1-5): ")
 
-                            # Add logic for shopping using self.cart_instance.viewInventory() and self.cart_instance.addItem()
+        if choice == '1':
+            cart.viewCart(user.getUserID()) 
+        elif choice == '2':
+            ISBN = input("Enter ISBN to add to the cart: ")
+            cart.addToCart(user.getUserID(), ISBN)
+        elif choice == '3':
+            ISBN = input("Enter ISBN to remove from the cart: ")
+            cart.removeFromCart(user.getUserID(), ISBN)
+        elif choice == '4':
+            cart.checkOut(user.getUserID(), inventory)
+        elif choice == '5':
+            return
+        else:
+            print("Invalid choice. Please enter a number between 1 and 5.")
 
-                        elif logInChoice == 3:
-                            os.system('cls')
-                            print("\n\tView Cart Information selected\n")
+def login():
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
 
-                            # Add logic for viewing cart information using self.cart_instance.displayCart()
+    if user.login(username, password):
+        print("Login successful.")
+        after_login_menu(user, inventory, cart)
+    else:
+        print("Login failed. Please check your username and password.")
 
-                            print("\n\tPlease choose an option")
-                            print("1. Remove from cart")
-                            print("2. Checkout")
-                            print("3. Go back")
-                            cartChoice = input("\nSelect an option: ")
+def create_account():
+    user.createAccount()
 
-                            if cartChoice.isdigit():
-                                cartChoice = int(cartChoice)
-                                if 1 <= cartChoice <= 3:
-                                    pass
-                                else:
-                                    print("Invalid choice. Please enter a number between 1 and 3.")
-                                    continue
-
-                            if cartChoice == 1:
-                                itemtodelete = input("Enter the item ID of the item you want to remove: ")
-                                itemtodelete = int(itemtodelete)
-
-                                # Add logic for removing item from cart using self.cart_instance.removeItem()
-
-                            elif cartChoice == 2:
-                                print("\tCheckout Selected\n")
-                                paymentInfo = input("Please enter your payment info: ")
-
-                                # Add logic for checkout using self.cart_instance.checkout()
-
-                                os.system('cls')
-                                print("Checkout Successful.")
-                                break
-
-                        # Add similar logic for other menu options
-
-                    if logInChoice == 7:
-                        print("\nLog Out Selected\n")
-                        os.system('cls')
-                        loggedIn = False
-                        break
-
-            elif choice == 2:
-                os.system('cls')
-                print("\n\tCreate account selected.")
-                self.user_instance.createAccount()
-                print("Account created.")
-                os.system('cls')
-
-            elif choice == 3:
-                os.system('cls')
-                print("\nQuit selected.")
-                self.conn.commit()
-                self.conn.close()
-                quit()
+def quit_program():
+    print("Exiting program.")
+    exit()
 
 if __name__ == "__main__":
-    menu_instance = Menu()
-    menu_instance.run()
+    inventory = Inventory('M&T_Database.db', 'Inventory')
+    user = User('M&T_Database.db', 'User')
+    cart = Cart('M&T_Database.db', 'Cart')
+
+    while True:
+        before_login_menu()
