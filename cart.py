cd377940882749cba97dda05efdfa3d75b6c91d6
Roshan Patel <149646948+RoshnP2@users.noreply.github.com>
2023-11-29 19:14:02 -0600

    Add files via upload

--- cart.py
+++ cart.py
@@ -1,4 +1,5 @@
 import sqlite3
+from inventory import Inventory
 
 class Cart:
     def __init__(self, database_name, table_name):
@@ -8,30 +9,88 @@ class Cart:
     def viewCart(self, userID):
         conn = sqlite3.connect(self.database_name)
         c = conn.cursor()
-        c.execute(f"SELECT * FROM {self.table_name} WHERE UserID=?", (userID,))
+        c.execute(f"SELECT ISBN, Quantity FROM {self.table_name} WHERE UserID=?", (userID,))
         cart_data = c.fetchall()
         conn.close()
 
-        return cart_data
+        if not cart_data:
+            print('Cart is empty')
+        else:
+            print("\nCart Information:")
+            print("ISBN\t\tQuantity")
+            print("------------------------")
+            for isbn, quantity in cart_data:
+                print(f"{isbn}\t\t{quantity}")
 
-    def addToCart(self, userID, ISBN, quantity):
+            return cart_data
+
+    def addToCart(self, userID, ISBN, quantity=1):
         conn = sqlite3.connect(self.database_name)
         c = conn.cursor()
 
-        c.execute(f"INSERT INTO {self.table_name} (UserID, ISBN, Quantity) VALUES (?, ?, ?)", (userID, ISBN, quantity))
+        # Check if the book is already in the cart
+        c.execute(f"SELECT Quantity FROM {self.table_name} WHERE UserID=? AND ISBN=?", (userID, ISBN))
+        existing_quantity = c.fetchone()
+
+        if existing_quantity:
+            # Book already in the cart, update the quantity
+            new_quantity = existing_quantity[0] + quantity
+            c.execute(f"UPDATE {self.table_name} SET Quantity=? WHERE UserID=? AND ISBN=?", (new_quantity, userID, ISBN))
+        else:
+            # Book not in the cart, insert a new entry
+            c.execute(f"INSERT INTO {self.table_name} (UserID, ISBN, Quantity) VALUES (?, ?, ?)", (userID, ISBN, quantity))
 
         conn.commit()
         conn.close()
+
 
     def removeFromCart(self, userID, ISBN):
         conn = sqlite3.connect(self.database_name)
         c = conn.cursor()
 
-        c.execute(f"DELETE FROM {self.table_name} WHERE UserID=? AND ISBN=?", (userID, ISBN))
+        # Check if the book is in the cart
+        c.execute(f"SELECT Quantity FROM {self.table_name} WHERE UserID=? AND ISBN=?", (userID, ISBN))
+        existing_quantity = c.fetchone()
+
+        if existing_quantity:
+            # Book in the cart, decrement the quantity
+            new_quantity = existing_quantity[0] - 1
+            if new_quantity > 0:
+                c.execute(f"UPDATE {self.table_name} SET Quantity=? WHERE UserID=? AND ISBN=?", (new_quantity, userID, ISBN))
+            else:
+                # If the quantity becomes 0, remove the entry from the cart
+                c.execute(f"DELETE FROM {self.table_name} WHERE UserID=? AND ISBN=?", (userID, ISBN))
 
         conn.commit()
         conn.close()
 
     def checkOut(self, userID):
-        # Add your logic for checkout here
-        pass
+        conn = sqlite3.connect(self.database_name)
+        c = conn.cursor()
+
+        try:
+            # Get cart items for the user
+            c.execute(f"SELECT ISBN, Quantity FROM {self.table_name} WHERE UserID=?", (userID,))
+            cart_items = c.fetchall()
+
+            if not cart_items:
+                print("Cart is empty. Nothing to check out.")
+                return
+
+            # Check out each item
+            for isbn, quantity in cart_items:
+                # Update inventory (assuming there is an Inventory class with decreaseStock function)
+                inventory = Inventory("M&T_Database.db", "inventory")
+                inventory.decreaseStock(isbn)
+
+            # Clear the cart
+            c.execute(f"DELETE FROM {self.table_name} WHERE UserID=?", (userID,))
+
+            print("Checkout successful!")
+
+        except sqlite3.Error as e:
+            print(f"SQLite error: {e}")
+
+        finally:
+            conn.commit()
+            conn.close()
--- inventory.py
+++ inventory.py
@@ -6,13 +6,29 @@ class Inventory:
         self.table_name = table_name
 
     def viewInventory(self):
-        conn = sqlite3.connect(self.database_name)
-        c = conn.cursor()
-        c.execute(f"SELECT * FROM {self.table_name}")
-        inventory_data = c.fetchall()
-        conn.close()
+        try:
+            conn = sqlite3.connect(self.database_name)
+            c = conn.cursor()
+            c.execute(f"SELECT ISBN, Title, Author, Genre, Pages, ReleaseDate, Stock FROM {self.table_name}")
+            inventory_data = c.fetchall()
 
-        return inventory_data
+            print("\nInventory Information:")
+            print("ISBN\tTitle\tAuthor\tGenre\tPages\tReleaseDate\tStock")
+            print("-----------------------------------------------------------------")
+            for isbn, title, author, genre, pages, release_date, stock in inventory_data:
+                # Handling None values for Title
+                title_str = title if title is not None else "None"
+                print(f"{isbn}\t{title_str}\t{author}\t{genre}\t{pages}\t{release_date}\t{stock}")
+
+            return inventory_data
+
+        except sqlite3.Error as e:
+            print(f"SQLite error: {e}")
+
+        finally:
+            conn.close()
+
+
 
     def searchInventory(self, title):
         conn = sqlite3.connect(self.database_name)
